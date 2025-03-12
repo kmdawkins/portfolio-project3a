e79cb7fa1e607860484df392d01b231740cad2e7
Katherina Marie Dawkins <km.dawkins@outlook.com>
2025-03-12 13:29:39 -0700

    feat: create staging table for pmo (transactions)

---
+++ sql_queries/transformations/create_staging_pmo.sql
@@ -0,0 +1,23 @@
+-- =================================================
+-- Script: create_staging_pmo.sql
+-- Purpose: Create staging table for raw PMO data ingestion
+-- Author: Katherina Dawkins (Project 3A - Python ETL)
+-- =================================================
+
+-- Drop table if it exists to avoid duplication errors.
+DROP TABLE IF EXISTS staging.pmo;
+
+-- Create staging table (without constraints or indexes)
+CREATE TABLE staging.pmo (
+    payment_no              VARCHAR(20),
+    transaction_date        DATE,
+    campaign_id             VARCHAR(10),
+    description             TEXT,
+    contract_no             VARCHAR(20),
+    purchase_order          VARCHAR(20),
+    purchase_requisition    VARCHAR(20),
+    project_no              VARCHAR(50),
+    payment_entity          VARCHAR(50),
+    amount_usd              NUMERIC(15,2),
+    amount_cny              NUMERIC(15,2)
+);
