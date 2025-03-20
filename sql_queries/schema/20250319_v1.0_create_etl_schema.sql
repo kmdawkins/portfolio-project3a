e080ff1bf0116f11c6630c33f504690454b00796
Katherina Marie Dawkins <km.dawkins@outlook.com>
2025-03-20 13:32:48 -0700

    feat: create etl schema for staging and transformations

---
+++ sql_queries/schema/20250319_v1.0_create_etl_schema.sql
@@ -0,0 +1,10 @@
+-- =================================================
+-- Script: 20250319_v1.0_create_etl_schema.sql
+-- Purpose: Create ETL schema for Project 3A
+-- Author: Katherina Dawkins
+-- Version: v1.0
+-- =================================================
+
+
+-- Create schema only if it doesn't already exist
+CREATE SCHEMA IF NOT EXISTS etl AUTHORIZATION postgres;
