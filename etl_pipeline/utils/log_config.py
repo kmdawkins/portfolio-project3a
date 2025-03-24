94881b0b4100d667983631998c7ffed77b6b58b7
Katherina Marie Dawkins <km.dawkins@outlook.com>
2025-03-24 15:29:09 -0700

    feat: add log_config.py utility to centralize logging config (and Airflow readiness)

---
+++ etl_pipeline/utils/log_config.py
@@ -0,0 +1,8 @@
+from loguru import logger
+import sys
+
+
+def setup_logger(log_path: str = "logs/default.log", level: str = "INFO"):
+    logger.remove()
+    logger.add(sys.stdout, level=level)
+    logger.add(log_path, level=level, rotation="10 MB", retention="7 days", compressuin="zip")
