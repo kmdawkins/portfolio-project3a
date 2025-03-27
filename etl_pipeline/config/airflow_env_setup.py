ede54d09a4270e7628c7e594159d36808acdead8
Katherina Marie Dawkins <km.dawkins@outlook.com>
2025-03-27 14:54:24 -0700

    fix(config): expand environment variables for configure/airflow_env_setup.py

--- etl_pipeline/config/airflow_env_setup.py
+++ etl_pipeline/config/airflow_env_setup.py
@@ -5,11 +5,14 @@ from dotenv import load_dotenv
 def configure_airflow_env():
     load_dotenv()
 
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+
     os.environ["AIRFLOW_HOME"] = os.getenv("AIRFLOW_HOME")
     os.environ["AIRFLOW_DATABASE_SQL_ALCHEMY_CONN"] = (
-        f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+        f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     )
     os.environ["AIRFLOW_CORE_EXECUTOR"] = "LocalExecutor"
-
-assert os.environ["AIRFLOW_HOME"], "AIRFLOW_HOME is not set"
